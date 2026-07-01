87e85a5229434c846b0309c3a13f107493d9b602
anamariass <anamariasoaressantana2@gmail.com>
2026-06-30 22:53:57 -0300

    Update fornecedor.models.js

--- models/fornecedor.models.js
+++ models/fornecedor.models.js
@@ -1,5 +1,6 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../config/db');
+const sequelize = require('../config/bd');
 
 const Fornecedor = sequelize.define('Fornecedor', {
     nome: {
